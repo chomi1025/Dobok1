6b11410d9e35f740802f27907893b25deb43a77b
chomi <chomi49100@gmail.com>
2026-03-27 18:57:23 +0900

    fix : session

--- src/components/header/AuthIcons.client.tsx
+++ src/components/header/AuthIcons.client.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import login from "@/assets/Image/header/mage_login.png";
@@ -8,28 +8,30 @@ import { Session } from "next-auth";
 import toast from "react-hot-toast";
 import { useState } from "react";
 
-interface Props {
-  session: Session | null;
-}
+export default function AuthIcons() {
+  const { data: session, status } = useSession();
 
-export default function AuthIcons({ session }: Props) {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
 
   const onClickSignout = async () => {
     if (isLoggingOut) return;
-
     try {
       setIsLoggingOut(true);
-
       const data = await signOut({
         redirect: false,
         callbackUrl: "/",
       });
 
-      toast.success(`로그아웃이 완료되었습니다`, { duration: 2000 });
+      toast.success("로그아웃이 완료되었습니다!\n메인화면으로 이동합니다.", {
+        duration: 2000,
+        style: {
+          whiteSpace: "pre-line",
+          textAlign: "center",
+        },
+      });
 
       setTimeout(() => {
-        window.location.href = data.url || "/";
+        window.location.href = data?.url || "/";
       }, 1500);
     } catch (error) {
       console.error("로그아웃 에러:", error);
@@ -38,14 +40,27 @@ export default function AuthIcons({ session }: Props) {
     }
   };
 
+  if (status === "loading") {
+    return (
+      <div style={{ width: 24, height: 24, opacity: 0.3 }}>
+        <Image src={login} alt="확인 중" width={24} height={24} priority />
+      </div>
+    );
+  }
+
+  // 로그인
   if (session) {
     return (
       <button
         type="button"
         onClick={onClickSignout}
+        disabled={isLoggingOut}
         style={{
           opacity: isLoggingOut ? 0.5 : 1,
           cursor: isLoggingOut ? "not-allowed" : "pointer",
+          border: "none",
+          background: "none",
+          padding: 0,
         }}
       >
         <Image
@@ -59,6 +74,7 @@ export default function AuthIcons({ session }: Props) {
     );
   }
 
+  // 비로그인
   return (
     <Link href="/login" prefetch={false}>
       <Image
